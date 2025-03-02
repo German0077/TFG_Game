e42ba57fb0927aa3104e9cfcd3954547f93b8fb0
Germán Fábregas Vacas <german.fabregasvacas@usp.ceu.es>
2025-03-02 12:23:15 +0100

    Player Health System

---
+++ Assets/Scripts/EnemyCombat.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class EnemyCombat : MonoBehaviour
+{
+
+    public int damage = 1;
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+        collision.gameObject.GetComponent<PlayerHealth>().HealthChange(-damage);
+    }
+
+
+}
---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    
+    public int currentHealth;
+    public int maxHealth = 5;
+
+    public SpriteRenderer playerSprite;
+    public PlayerMovement playerMovement;
+
+    // Start is called once before the first execution of Update
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    public void HealthChange(int amount) {
+        currentHealth += amount;
+
+        if (currentHealth <= 0) {
+            playerSprite.enabled = false;
+            playerMovement.enabled = false;
+        }
+    }
+}
